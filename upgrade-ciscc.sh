3038540c8444187a7d97629e9bd516a4ad2f3773
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-01 02:42:38 +0800

    feat!(upgrade-ciscc): improve
    note that you may need to use "source" to import the function now

--- upgrade-ciscc.sh
+++ upgrade-ciscc.sh
@@ -9,8 +9,29 @@ pull_images() {
     docker-compose pull
 }
 
+regenerate_strapi() {
+    container=$(docker run --entrypoint "bash" -v "$(pwd)/app:/app" node:14)
+
+    docker exec "$container" cd /app
+    docker exec "$container" export NODE_ENV=production
+    docker exec "$container" yarn && yarn build
+
+    docker container stop "$container"
+    docker container rm "$container"
+}
+
 recreate_containers() {
     docker-compose down && docker-compose up -d
 }
 
-pull_repo && pull_images && recreate_containers
+pull_all() {
+    pull_repo && pull_images
+}
+
+quick_upgrade_ciscc() {
+    pull_all && recreate_containers
+}
+
+full_upgrade_ciscc() {
+    pull_all && regenerate_strapi && recreate_containers
+}
